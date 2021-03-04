332793d07ba763ab66fc79ec4d50def53b54ea6c
martingrx1 <52628346+martingrx1@users.noreply.github.com>
2021-03-05 00:38:57 +0800

    Delete api.js

--- env/api.js
+++
@@ -1,9 +0,0 @@
-
-const WX_CONF = {
-  ACCESS_TOKEN_API : `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&`,
-  SEND_SUBSCRIBEMSG_API : `https://api.weixin.qq.com/cgi-bin/message/subscribe/send?`
-
-}
-
-
-export default  WX_CONF 
